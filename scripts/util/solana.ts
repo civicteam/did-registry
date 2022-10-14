ace88451889f8eed27c754a709c52b00085b8e1e
dankelleher <dankelleher@yahoo.com>
2022-10-14 14:26:06 +0200

    Added some scripts to help register DIDs

---
+++ scripts/util/solana.ts
@@ -0,0 +1,26 @@
+import os from "os";
+import { Registry } from "../../src";
+import { Connection, Keypair, Transaction } from "@solana/web3.js";
+
+const secretKey = require(os.homedir() + "/.config/solana/id.json");
+const keypair = Keypair.fromSecretKey(Buffer.from(secretKey));
+
+const toWallet = (keypair: Keypair) => ({
+  publicKey: keypair.publicKey,
+  signTransaction: async (transaction: Transaction) => {
+    transaction.partialSign(keypair);
+    return transaction;
+  },
+  signAllTransactions: async (transactions: Transaction[]) => {
+    transactions.forEach((t) => t.partialSign(keypair));
+    return transactions;
+  },
+});
+
+export const registry = new Registry(
+  toWallet(keypair),
+  new Connection(
+    process.env.RPC_URL || "https://api.devnet.solana.com",
+    "confirmed"
+  )
+);
